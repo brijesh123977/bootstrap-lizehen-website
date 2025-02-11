88a46994cef31c3a8ae5a35db182fa6d948735df
Brijesh Shiroya <brijesh.shiroya.reversebits@gmail.com>
2025-02-11 19:36:26 +0530

    margin add

--- script.js
+++ script.js
@@ -2,19 +2,3 @@ function changeImage(fileName){
     let img = document.querySelector("#bannerImage");
     img.setAttribute("src", fileName);
 }
-
-
-
-// $(document).ready(function () {
-//     $(window).scroll(function () {
-//       if (this.scrollY > 20) {
-//         $(".navbar").addClass("sticky");
-//       } else {
-//         $(".navbar").removeClass("sticky");
-//       }
-//       if (this.scrollY > 500) {
-//         $(".scroll-up-btn").addClass("show");
-//       } else {
-//         $(".scroll-up-btn").removeClass("show");
-//       }
-//     });
