84f64981580b8468ce16711893678cce76b00c98
Brijesh Shiroya <brijesh.shiroya.reversebits@gmail.com>
2025-02-17 16:58:22 +0530

    active tag add..

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function changeImage(fileName){
     img.setAttribute("src", fileName);
 }
 
+
+
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("button-word");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
